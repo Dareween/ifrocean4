f5c02c551f176913b7475c75038807ab02d3d83d
Vincent Gicquel <vincentgicquel@free.fr>
2016-07-15 16:46:24 +0200

    mise à jour fichier config

--- Ifrocean_BDD/Config.php
+++ Ifrocean_BDD/Config.php
@@ -3,7 +3,7 @@
 
 class Config {
     const SERVERNAME = "localhost";
-    const DBNAME = "db_ifrocean_apm";
+    const DBNAME = "db_ifrocean";
     const USERNAME = "ifrocean4";
     const PASSWORD = "ifrocean4";
     
